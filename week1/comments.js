36db713aac9f818fad1b958aadfd8f3183e90001
shahid <shahidansari9876@gmail.com>
2026-07-28 21:46:38 +0530

    initial commit

---
+++ week1/comments.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+console.log("my first comment line")
+console.log("lets start learning the automated testing with playwright and node js")
+
+// lets run the code for automated testing with javascript playwright 
+
+/* learnign the mcp server with playwright and node js for automated testing */
+
+/* first learn javascript fundamentals  and than will go for the automation testing 
+with playwright and also learn mcp  */
+
+
+/* 1. what is node js and how it works
+   2. what is playwright and how it works
+   3. what is mcp server and how it works
+   4. how to run the automated testing with playwright and node js
+   5. how to run the automated testing with playwright and mcp server
+   6. how to run the automated testing with playwright and mcp server and node js
+   7. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals
+   8. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals and learnings
+   9. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals and learnings and best practices
+   10. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals and learnings and best practices and tips
+   */ 
