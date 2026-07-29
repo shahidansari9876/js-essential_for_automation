efac6f4cc760d4399c77299f25937f51d8a7634c
SHAHID ANSARI <shahidanari9876@gmail.com>
2026-07-29 22:30:06 +0530

    variable

--- week1/comments.js
+++ week1/comments.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#! /usr/bin/env node learn start js essential  
 
 console.log("my first comment line")
 console.log("lets start learning the automated testing with playwright and node js")
@@ -7,7 +7,7 @@ console.log("lets start learning the automated testing with playwright and node 
 
 /* learnign the mcp server with playwright and node js for automated testing */
 
-/* first learn javascript fundamentals  and than will go for the automation testing 
+/* first learn javascript essential  and than will go for the automation testing 
 with playwright and also learn mcp  */
 
 
@@ -17,8 +17,8 @@ with playwright and also learn mcp  */
    4. how to run the automated testing with playwright and node js
    5. how to run the automated testing with playwright and mcp server
    6. how to run the automated testing with playwright and mcp server and node js
-   7. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals
-   8. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals and learnings
-   9. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals and learnings and git push -u origin main practices
-   10. how to run the automated testing with playwright and mcp server and node js and javascript fundamentals and learnings and best practices and tips
+   7. how to run the automated testing with playwright and mcp server and node js and javascript essential
+   8. how to run the automated testing with playwright and mcp server and node js and javascript essential and learnings
+   9. how to run the automated testing with playwright and mcp server and node js and javascript essential and learnings and git push -u origin main practices
+   10. how to run the automated testing with playwright and mcp server and node js and javascript essential and learnings and best practices and tips
    */ 
---
+++ week1/variable.js
@@ -0,0 +1,53 @@
+#! variable and it scope in javascript learing, 
+
+let a=10 // here a is variable and 10 is value of variable a and let is used to declare variable a
+let b=20// here b is variable and 20 is value of variable b
+
+let c=a+b //here c is variable and a+b is value of variable c
+console.log(c) //here console.log is used to print the value of variable c
+
+var d='hello' // here d is variable and 'hello' is value of variable d and var is used to declare variable d
+var e='world' // here e is variable and 'world' is value of variable e
+
+const f=30 // here f is variable and 30 is value of variable f and const is used to declare variable 
+
+console.log(d+e) //here console.log is used to print the value of variable d and e
+console.log(f) //here console.log is used to print the value of variable f
+
+
+
+/** here we are going to discuss about variable scope in javascript
+ * where var is function scope and let and const are block scope
+ * var keep the value as global and let and const keep the value as local
+ * like var run the code with identifier with just above declaration 
+ * and let and const run the code with identifier with just below declaration or block lever declaration
+ */
+// lets take an example of variable scope
+var x=100
+
+if(true){
+    var x=200 // here x is variable and 200 is value of variable x and var is used to declare variable x
+    console.log("here var scope of if condtion is the value of x: ", x) //here console.log is used to print the value of variable x
+}
+console.log("here var scope outside of if condtion is the value of x: ", x) //here console.log is used to print the value of variable x   
+
+
+
+//lets take an example let variable scope
+let y=100
+
+if(true){
+    let y=200 // here y is variable and 200 is value of variable y and let is used to declare variable y
+    console.log("here let scope of if condtion is the value of y: ", y) //here console.log is used to print the value of variable y
+}
+console.log("here let scope outside of if condtion is the value of y: ", y) //here console.log is used to print the value of variable y   
+
+
+//lets take an example const variable scope
+const z=100
+
+if(true){
+    const z=200 // here z is variable and 200 is value of variable z and const is used to declare variable z
+    console.log("here const scope of if condtion is the value of z: ", z) //here console.log is used to print the value of variable z
+}
+console.log("here const scope outside of if condtion is the value of z: ", z) //here console.log is used to print the value of variable z   
