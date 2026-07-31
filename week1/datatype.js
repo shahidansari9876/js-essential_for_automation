3e4b5d9f7b896a2d10b8cf3ee0fa38fe6da46f7b
SHAHID ANSARI <shahidanari9876@gmail.com>
2026-07-31 22:47:11 +0530

    datatype

---
+++ week1/datatype.js
@@ -0,0 +1,23 @@
+#! datatype learning for js essentials
+
+//typeof operator in js is used to find the datatype of variable
+let a=10 // here a is variable and 10 is value of variable a and let is used to declare variable a
+let b='hello' // here b is variable and 'hello' is value of variable b and let is used to declare variable b
+let c=true // here c is variable and true is value of variable c and let is used to declare variable c
+let d=undefined // here d is variable and undefined is value of variable d and let is used to declare variable d
+let e=null // here e is variable and null is value of variable e and let is used to declare variable e
+
+console.log("datatype of a: ", typeof a) //here console.log is used to print the datatype of variable a
+console.log("datatype of b: ", typeof b) //here console.log is used to print the datatype of variable b
+console.log("datatype of c: ", typeof c) //here console.log is used to print the datatype of variable c
+console.log("datatype of d: ", typeof d) //here console.log is used to print the datatype of variable d
+console.log("datatype of e: ", typeof e) //here console.log is used to print the datatype of variable e
+console.log("datatype of f: ", typeof ("shahid")) // here console.log is used to print the datatype of variable f
+
+// datatypeforms in js essentials 
+
+console.log("shahid ansari") // literals dataform 
+let str='hello' // variable dataforms
+console.log("string literal: ", str) 
+
+console.log("shahid" + " ansari") // string concatenation dataforms , or expression dataforms
--- week1/variable.js
+++ week1/variable.js
@@ -1,4 +1,4 @@
-#! variable and it scope in javascript learing, 
+#! variable and it scope in javascript learing, and literals 
 
 let a=10 // here a is variable and 10 is value of variable a and let is used to declare variable a
 let b=20// here b is variable and 20 is value of variable b
@@ -51,3 +51,17 @@ if(true){
     console.log("here const scope of if condtion is the value of z: ", z) //here console.log is used to print the value of variable z
 }
 console.log("here const scope outside of if condtion is the value of z: ", z) //here console.log is used to print the value of variable z   
+
+
+//literals in js
+let str='hello'  // here the literal is string and the value of str is 'hello' and let is used to declare variable str
+let num=10 // here the literal is number and the value of num is 10 and let is used to declare variable num
+let bool=true // here the literal is boolean and the value of bool is true and let is used to declare variable bool
+let undef=undefined  // here the literal is undefined and the value of undef is undefined and let is used to declare variable undef
+let nul=null  // here the literal is null and the value of nul is null and let is used to declare variable nul  
+
+console.log("string literal: ", str)
+console.log("number literal: ", num)
+console.log("boolean literal: ", bool)
+console.log("undefined literal: ", undef)
+console.log("null literal: ", nul)
